0d06e3f36010644e44b796047cd529516ca80c48
Anik Shakya <aniklinkin@gmail.com>
2024-07-29 17:06:55 +0545

    - fixed permission request issue

--- lib/src/services/permission_service.dart
+++ lib/src/services/permission_service.dart
@@ -3,7 +3,6 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionService {
   // Request required permissions if they are not already granted
   Future<void> checkAndRequestPermissions() async {
-    
     final permissions = [
       Permission.manageExternalStorage,
       Permission.audio,
@@ -12,18 +11,11 @@ class PermissionService {
       Permission.notification
     ];
 
-    // Check current permission statuses
-    final statuses = await Future.wait(permissions.map((p) => p.status));
-    
-    // Determine which permissions are not granted
-    final permissionsToRequest = [
-      for (int i = 0; i < permissions.length; i++)
-        if (!statuses[i].isGranted) permissions[i]
-    ];
-
-    if (permissionsToRequest.isNotEmpty) {
-      // Request permissions that are not granted
-      await Future.wait(permissionsToRequest.map((p) => p.request()));
+    for (var permission in permissions) {
+      var status = await permission.status;
+      if (!status.isGranted) {
+        await permission.request();
+      }
     }
   }
 }
